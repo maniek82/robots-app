7e55ec1f55efffc121cfe043d7a45184aff57db6
maniek82 <mariusz.tripler@gmail.com>
2018-09-05 10:47:42 +0100

    changed scroll size

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -3,7 +3,7 @@ import React,{Component} from 'react';
 class Scroll extends Component {
     render() {
         return (
-            <div className="mt4" style={{overflowY: 'hidden',border: '3px solid black',height: '370px'}}>
+            <div className="mt4" style={{overflowY: 'hidden',border: '3px solid black',height: '400px'}}>
                 {this.props.children}
             </div>
         )
